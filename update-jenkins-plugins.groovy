90cd824bb13789d2fd783b19e90662df4ef37f47
Román Martín <romanmartin@paradigmadigital.com>
2023-02-16 10:31:52 +0100

    Fix artifacts path

--- update-jenkins-plugins.groovy
+++ update-jenkins-plugins.groovy
@@ -21,9 +21,9 @@ pipeline {
             steps {
                 script {
                     def safePluginUpdateModule = load("scripts/jenkins-plugins-uptodate.groovy")
-                    safePluginUpdateModule.list_jenkins_plugins("${WORKSPACE}/admin-jobs/Update-Jenkins-Plugins", "plugins_list_BEFORE-UPDATE_${DATETIME}.txt")
+                    safePluginUpdateModule.list_jenkins_plugins("${WORKSPACE}/jenkins_auto_update_plugins", "plugins_list_BEFORE-UPDATE_${DATETIME}.txt")
                     (pluginsToReviewManually, pluginsDeprecated) = safePluginUpdateModule.jenkins_safe_plugins_update()
-                    safePluginUpdateModule.list_jenkins_plugins("${WORKSPACE}/admin-jobs/Update-Jenkins-Plugins", "plugins_list_AFTER-UPDATE_${DATETIME}.txt")
+                    safePluginUpdateModule.list_jenkins_plugins("${WORKSPACE}/jenkins_auto_update_plugins", "plugins_list_AFTER-UPDATE_${DATETIME}.txt")
                 }
             }
         }
